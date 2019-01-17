072bdea147b789b88fcb44f1b7c781c0b32798fa
libertyernie <maybeway36@gmail.com>
2019-01-17 15:19:20 -0600

    Split SavedDeltaEntry into separate file

---
+++ DeviantArtFs.Stash.Marshal/SavedDeltaEntry.fs
@@ -0,0 +1,17 @@
+﻿namespace DeviantArtFs.Stash.Marshal
+
+open System
+open DeviantArtFs
+
+type SavedDeltaEntry =
+    {
+        Itemid: Nullable<int64>
+        Stackid: int64
+        MetadataJson: string
+        Position: int
+    }
+    interface DeviantArtFs.ISerializedStashDeltaEntry with
+        member this.Itemid = this.Itemid
+        member this.Stackid = this.Stackid |> Nullable
+        member this.MetadataJson = this.MetadataJson
+        member this.Position = this.Position |> Nullable
--- DeviantArtFs.Stash.Marshal/StashNode.fs
+++ DeviantArtFs.Stash.Marshal/StashNode.fs
@@ -1,20 +1,6 @@
 namespace DeviantArtFs.Stash.Marshal
 
-open System
 open DeviantArtFs
-
-type SavedDeltaEntry =
-    {
-        Itemid: Nullable<int64>
-        Stackid: int64
-        MetadataJson: string
-        Position: int
-    }
-    interface DeviantArtFs.ISerializedStashDeltaEntry with
-        member this.Itemid = this.Itemid
-        member this.Stackid = this.Stackid |> Nullable
-        member this.MetadataJson = this.MetadataJson
-        member this.Position = this.Position |> Nullable
 
 type StashNode(root: IStashRoot, metadata: StashMetadata) =
     member val Metadata = metadata with get, set
