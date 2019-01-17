19276aeffbf204c7a91a1557e97339f4aa14ac40
libertyernie <maybeway36@gmail.com>
2019-01-17 15:45:11 -0600

    add code

---
+++ DeviantArtFs.Stash.Marshal.Examples.StashInterface/Models/StashEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviantArtFs.Stash.Marshal.Examples.StashInterface.Models
+{
+    public class StashEntry : ISerializedStashDeltaEntry
+    {
+        public int StashEntryId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public long? ItemId { get; set; }
+
+        public long StackId { get; set; }
+
+        [Required]
+        public string MetadataJson { get; set; }
+
+        public int Position { get; set; }
+
+        long? ISerializedStashDeltaEntry.Itemid => ItemId;
+
+        long? ISerializedStashDeltaEntry.Stackid => StackId;
+
+        string ISerializedStashDeltaEntry.MetadataJson => MetadataJson;
+
+        int? ISerializedStashDeltaEntry.Position => Position;
+    }
+}
--- DeviantArtFs.Stash.Marshal.Examples.StashInterface/Startup.cs
+++ DeviantArtFs.Stash.Marshal.Examples.StashInterface/Startup.cs
@@ -2,19 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeviantArtFs.Stash.Marshal.Examples.StashInterface.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DeviantArtFs.Stash.Marshal.Examples.StashInterface
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public static int DeviantArtClientId { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ExampleDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton(
+                typeof(DeviantArtAuth),
+                new DeviantArtAuth(
+                    int.Parse(Configuration["DeviantArtClientId"]),
+                    Configuration["DeviantArtClientSecret"]));
+            DeviantArtClientId = int.Parse(Configuration["DeviantArtClientId"]);
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromHours(1);
+                options.Cookie.HttpOnly = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +56,21 @@ namespace DeviantArtFs.Stash.Marshal.Examples.StashInterface
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
             }
 
-            app.Run(async (context) =>
+            app.UseSession();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
